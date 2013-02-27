9d0e0f2f0f5ca1a145d60b9f373c537118abac28
Michael C. Toren <mct@toren.net>
2013-02-26 21:25:34 -0800

    Use HMAC

--- ballots.py
+++ ballots.py
@@ -5,7 +5,7 @@
 __author__  = "Leif Ryge"
 __license__ = "WTFPL 2012-2013 No Rights Reserved"
 
-import sys, random, hashlib
+import sys, random, hashlib, hmac
 
 def print_ballots( count, template, candidates, secret ):
 
@@ -18,11 +18,14 @@ def print_ballots( count, template, candidates, secret ):
     for i in range( count ):
         random.shuffle( candidates )
         ballot = "\n".join( "[ ] " + c for c in candidates )
-        ballots[ hashlib.sha1(ballot+secret).hexdigest()[:6] ] = ballot
+        key = hmac.new(secret, msg=ballot, digestmod=hashlib.sha1).digest().encode("base64")
+        ballots[key] = ballot
 
     for hash, ballot in ballots.items():
         print template % (hash, ballot)
 
 if __name__ == "__main__":
+    if len(sys.argv[1:]) != 4:
+        print "Usage: %s <count> <ballot_template> <candidates_file> <secret_file>" % sys.argv[0]
+        sys.exit(1)
     print_ballots( *sys.argv[1:] )
-
